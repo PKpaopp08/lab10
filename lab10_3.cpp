b571d8d580b53177b205065ef2e17c422a8ba103
PAO <natthachai_bubpa@cmu.ac.th>
2023-12-22 01:22:44 +0700

    final

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include<fstream>
+
+
+using namespace std;
+
+int main(){
+    
+    int count = 0 ;
+    float sum = 0 ;
+    float SOPS = 0;
+    string textline;
+    ifstream source("score.txt");
+    
+    while (getline(source,textline))
+    {
+        sum += atof(textline.c_str());
+        SOPS += pow(atof(textline.c_str()),2);
+        count++;
+    }
+    
+    float mean = sum/count;
+    cout << "Number of data = "<<count<< "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = "<< sqrt(SOPS/count-(pow(mean,2)));
+}
